e0b0c3ebc473a20742b48cf9067e6ed3a8056dba
ArgentumWalker <svidchen-ko@mail.ru>
2022-10-13 23:19:55 +0300

    HW 01

---
+++ hw01_lunar_lander/agent.py
@@ -0,0 +1,13 @@
+import random
+import numpy as np
+import os
+import torch
+
+
+class Agent:
+    def __init__(self):
+        self.model = torch.load(__file__[:-8] + "/agent.pkl")
+        
+    def act(self, state):
+        return 0 # TODO
+
---
+++ hw01_lunar_lander/train.py
@@ -0,0 +1,109 @@
+from gym import make
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.optim import Adam
+from collections import deque
+import random
+import copy
+
+GAMMA = 0.99
+INITIAL_STEPS = 1024
+TRANSITIONS = 500000
+STEPS_PER_UPDATE = 4
+STEPS_PER_TARGET_UPDATE = STEPS_PER_UPDATE * 1000
+BATCH_SIZE = 128
+LEARNING_RATE = 5e-4
+
+
+class DQN:
+    def __init__(self, state_dim, action_dim):
+        self.steps = 0 # Do not change
+        self.model = None # Torch model
+
+    def consume_transition(self, transition):
+        # Add transition to a replay buffer.
+        # Hint: use deque with specified maxlen. It will remove old experience automatically.
+        pass
+
+    def sample_batch(self):
+        # Sample batch from a replay buffer.
+        # Hints:
+        # 1. Use random.randint
+        # 2. Turn your batch into a numpy.array before turning it to a Tensor. It will work faster
+        pass
+        
+    def train_step(self, batch):
+        # Use batch to update DQN's network.
+        pass
+        
+    def update_target_network(self):
+        # Update weights of a target Q-network here. You may use copy.deepcopy to do this or 
+        # assign a values of network parameters via PyTorch methods.
+        pass
+
+    def act(self, state, target=False):
+        # Compute an action. Do not forget to turn state to a Tensor and then turn an action to a numpy array.
+        state = np.array(state)
+        return 0
+
+    def update(self, transition):
+        # You don't need to change this
+        self.consume_transition(transition)
+        if self.steps % STEPS_PER_UPDATE == 0:
+            batch = self.sample_batch()
+            self.train_step(batch)
+        if self.steps % STEPS_PER_TARGET_UPDATE == 0:
+            self.update_target_network()
+        self.steps += 1
+
+    def save(self):
+        torch.save(self.model, "agent.pkl")
+
+
+def evaluate_policy(agent, episodes=5):
+    env = make("LunarLander-v2")
+    returns = []
+    for _ in range(episodes):
+        done = False
+        state = env.reset()
+        total_reward = 0.
+        
+        while not done:
+            state, reward, done, _ = env.step(agent.act(state))
+            total_reward += reward
+        returns.append(total_reward)
+    return returns
+
+if __name__ == "__main__":
+    env = make("LunarLander-v2")
+    dqn = DQN(state_dim=env.observation_space.shape[0], action_dim=env.action_space.n)
+    eps = 0.1
+    state = env.reset()
+    
+    for _ in range(INITIAL_STEPS):
+        action = env.action_space.sample()
+
+        next_state, reward, done, _ = env.step(action)
+        dqn.consume_transition((state, action, next_state, reward, done))
+        
+        state = next_state if not done else env.reset()
+        
+    
+    for i in range(TRANSITIONS):
+        #Epsilon-greedy policy
+        if random.random() < eps:
+            action = env.action_space.sample()
+        else:
+            action = dqn.act(state)
+
+        next_state, reward, done, _ = env.step(action)
+        dqn.update((state, action, next_state, reward, done))
+        
+        state = next_state if not done else env.reset()
+        
+        if (i + 1) % (TRANSITIONS//100) == 0:
+            rewards = evaluate_policy(dqn, 5)
+            print(f"Step: {i+1}, Reward mean: {np.mean(rewards)}, Reward std: {np.std(rewards)}")
+            dqn.save()
